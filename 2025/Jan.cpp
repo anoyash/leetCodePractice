eda1acb5830bfb1d8b54d89acfbb3543edea5c59
Ashutosh Kumar <ak2417942@gmail.com>
2025-01-02 21:19:51 +0530

    2nd Jan leetcode

--- 2025/Jan.cpp
+++ 2025/Jan.cpp
@@ -26,6 +26,41 @@ public:
 };
 
 // 2nd January 2025
+class Solution {
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        vector<int> ans;
+        vector<pair<int,bool>> vowelsCnt;
+        int idx = 0,prev=0;
+
+        for(auto word : words ){
+            char start = word[0];
+            char end   = word[word.size()-1];
+
+            if( 
+                (start == 'a' || start == 'e' || start == 'i' || start == 'o' || start == 'u')
+                && 
+                (end=='a' || end == 'e' || end == 'i' || end =='o' || end == 'u')
+             ) 
+                vowelsCnt.push_back({prev+1,true});
+             else
+                vowelsCnt.push_back({prev,false});
+            
+            prev = vowelsCnt[idx].first;
+            idx++;
+        }
+
+        for (auto query : queries){
+            int left   = query[0];
+            int right  = query[1];
+            ans.push_back(vowelsCnt[right].first-vowelsCnt[left].first+vowelsCnt[left].second);
+        }
+
+        return ans;
+    }
+};
+
+
 // 3rd January 2025
 // 4th January 2025
 // 5th January 2025
